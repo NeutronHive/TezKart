655c79a32f0a760cf46952a6c8908fd476c877dd
ThunderSmoker <pitlawararadhya@gmail.com>
2023-08-26 17:17:48 +0530

    icon

--- ECommerce-Website/client/src/Components/About.jsx
+++ ECommerce-Website/client/src/Components/About.jsx
@@ -5,7 +5,7 @@ import { GitHub, TravelExplore } from '@mui/icons-material';
 const About = () => {
     return (
         <div className="we-are-block">
-            <div id="about-us-section">
+            <div id="about-us-section" style={{backgroundColor:"#630365"}}>
                 <div className="about-us-image">
                     <img
                         src="./team1.jpg"
@@ -19,7 +19,7 @@ const About = () => {
                     <p style={{color:'whitesmoke'}}>
  We are a dynamic team of dedicated individuals and our mission is to bring innovative ideas to life through technology. We're committed to embracing challenges, collaborating effectively, and delivering solutions that make a meaningful impact. With a focus on continuous learning and growth, we aim to carve a path towards excellence in the field of computer science and beyond.
                     </p>
-                    <a href="https://github.com/NeutronHive/FLipGem" target='_blank' style={{backgroundColor:"white",padding:"5px",display:"flex", alignItems:'center', color:'black', fontSize:'14px', justifyContent:'center'}} title="About Us Button">
+                    <a href="https://github.com/NeutronHive/TezKart" target='_blank' style={{backgroundColor:"white",padding:"5px",display:"flex", alignItems:'center', color:'black', fontSize:'14px', justifyContent:'center'}} title="About Us Button">
                         <div style={{fontWeight:'bolder', fontFamily:'monospace'}}>View GitHub</div>
                         <GitHub style={{fontSize:"1.5rem", marginLeft:'4px'}}/>
                     </a>
@@ -28,15 +28,15 @@ const About = () => {
             <div id="history-section">
                 <div className="history-image">
                     <img
-                        src="./griidproj1.jpeg"
+                        src="./griidproj1.png"
                         width="808"
                         height="471"
                         alt="Building Pic"
                     />
                 </div>
                 <div className="history-info">
-                    <h2 style={{fontFamily:'Inconsolata, monospace'}} >flipKart Grid 5.0 Project</h2>
-                    <p>
+                    <h2 style={{fontFamily:'Inconsolata, monospace',color:"#222"}} >TezAsia Hackathon Project</h2>
+                    <p style={{color:"#222"}}>
                     Our blockchain-based platform enhances E-commerce loyalty programs through transparent rewards, utilizing fungible tokens for engagement. Brands and partners can seamlessly manage tokens, while users track their rewards. The user-friendly blue-gold portal simplifies token issuance and offers a verifiable, scalable solution.
                     </p>
                     <a href="/" title="History Button" style={{backgroundColor:"#111",color:'whitesmoke',padding:"5px",display:"flex", alignItems:'center', fontSize:'14px', justifyContent:'center'}}>
